4887e0a115a6aef5f377d4daea200eda244cc056
Arun J <arunj123@gmail.com>
2026-02-22 14:42:36 +0100

    Add .gitignore and remove build artifacts from history

--- build/CMakeFiles/nuc_display.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for nuc_display.
-# This may be replaced when dependencies are built.
--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/arun/projects/nuc_display
-# Build directory: /home/arun/projects/nuc_display/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
